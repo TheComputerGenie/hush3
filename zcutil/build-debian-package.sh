91fdc1991c43d22770f62a67410517bee0c79013
Duke Leto <duke@leto.net>
2019-09-23 22:46:34 -0400

    Add sapling params to debian package and prevent some lintian warnings

--- zcutil/build-debian-package.sh
+++ zcutil/build-debian-package.sh
@@ -51,7 +51,8 @@ DEB_BIN=$BUILD_DIR/usr/bin
 DEB_CMP=$BUILD_DIR/usr/share/bash-completion/completions
 DEB_DOC=$BUILD_DIR/usr/share/doc/$PACKAGE_NAME
 DEB_MAN=$BUILD_DIR/usr/share/man/man1
-mkdir -p $BUILD_DIR/DEBIAN $DEB_CMP $DEB_BIN $DEB_DOC $DEB_MAN
+DEB_SHR=$BUILD_DIR/usr/share/hush
+mkdir -p $BUILD_DIR/DEBIAN $DEB_CMP $DEB_BIN $DEB_DOC $DEB_MAN $DEB_SHR
 chmod 0755 -R $BUILD_DIR/*
 
 # Package maintainer scripts (currently empty)
@@ -70,18 +71,24 @@ strip $DEB_BIN/hush-komodo-tx
 cp $SRC_PATH/src/hushd $DEB_BIN
 cp $SRC_PATH/src/hush-cli $DEB_BIN
 cp $SRC_PATH/src/hush-tx $DEB_BIN
+cp $SRC_PATH/sapling-output.params $DEB_SHR
+cp $SRC_PATH/sapling-spend.params $DEB_SHR
 
-echo "Copying docs"
+# Docs
 #cp $SRC_PATH/doc/release-notes/release-notes-1.0.0.md $DEB_DOC/changelog
 cp $SRC_PATH/contrib/debian/changelog $DEB_DOC
 #cp $SRC_DEB/changelog $DEB_DOC/changelog.Debian
 cp $SRC_DEB/copyright $DEB_DOC
 cp -r $SRC_DEB/examples $DEB_DOC
 
-echo "Copying manpages"
+# Manpages
 cp $SRC_DOC/man/komodod.1 $DEB_MAN/hushd.1
 cp $SRC_DOC/man/komodo-cli.1 $DEB_MAN/hush-cli.1
 cp $SRC_DOC/man/komodo-tx.1 $DEB_MAN/hush-tx.1
+# prevents warnings about binaries without manpages
+cp $SRC_DOC/man/komodod.1 $DEB_MAN/hush-komodod.1
+cp $SRC_DOC/man/komodo-cli.1 $DEB_MAN/hush-komodo-cli.1
+cp $SRC_DOC/man/komodo-tx.1 $DEB_MAN/hush-komodo-tx.1
 
 # Copy bash completion files
 cp $SRC_PATH/contrib/hushd.bash-completion $DEB_CMP/hushd
@@ -92,6 +99,9 @@ gzip --best -n $DEB_DOC/changelog
 gzip --best -n $DEB_MAN/hushd.1
 gzip --best -n $DEB_MAN/hush-cli.1
 gzip --best -n $DEB_MAN/hush-tx.1
+gzip --best -n $DEB_MAN/hush-komodod.1
+gzip --best -n $DEB_MAN/hush-komodo-cli.1
+gzip --best -n $DEB_MAN/hush-komodo-tx.1
 
 cd $SRC_PATH/contrib
 
