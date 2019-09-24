e2d8a7c0205ba4e93e1fc664508769fe1138dc1d
Duke Leto <duke@leto.net>
2019-09-23 20:42:58 -0400

    Greatly improve debian package generation script

--- zcutil/build-debian-package.sh
+++ zcutil/build-debian-package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-## Usage:
-##  ./zcutil/build-debian-package.sh
+# Copyright 2019 The Hush Developers
+# Usage: ./zcutil/build-debian-package.sh
 
 set -e
 set -x
@@ -13,21 +13,37 @@ SRC_DOC=$SRC_PATH/doc
 
 umask 022
 
+if [ ! -e "./src/komodod" ]; then
+	echo "No komodod found!"
+	echo "Make sure to run ./build.sh before running $0"
+	exit 1
+fi
+
+if [ ! -e "./src/komodo-cli" ]; then
+	echo "No komodo-cli found!"
+	echo "Make sure to run ./build.sh before running $0"
+	exit 1
+fi
+
 if [ ! -d $BUILD_PATH ]; then
+    echo "Creating $BUILD_PATH"
     mkdir $BUILD_PATH
 fi
 
-## PACKAGE_VERSION=$($SRC_PATH/src/hushd --version | grep version | cut -d' ' -f4 | tr -d v)
-## Need version setting from environment
-
+#PACKAGE_VERSION=$($SRC_PATH/src/hushd --version | grep version | cut -d' ' -f4 | tr -d v)
 PACKAGE_VERSION=3.1.0
 
 ##
-## Also, what does the sed end up doing?
 DEBVERSION=$(echo $PACKAGE_VERSION | sed 's/-beta/~beta/' | sed 's/-rc/~rc/' | sed 's/-/+/')
 BUILD_DIR="$BUILD_PATH/$PACKAGE_NAME-$PACKAGE_VERSION-amd64"
 
+if [ -e "$PACKAGE_NAME-$PACKAGE_VERSION-amd64.deb "]; then
+	echo "Backing up previous build"
+	cp $PACKAGE_NAME-$PACKAGE_VERSION-amd64.deb $PACKAGE_NAME-$PACKAGE_VERSION-amd64.deb.bak
+fi
+
 if [ -d $BUILD_DIR ]; then
+    echo "Removing $BUILD_DIR"
     rm -R $BUILD_DIR
 fi
 
@@ -43,21 +59,30 @@ chmod 0755 -R $BUILD_DIR/*
 #cp $SRC_DEB/postrm $BUILD_DIR/DEBIAN
 #cp $SRC_DEB/preinst $BUILD_DIR/DEBIAN
 #cp $SRC_DEB/prerm $BUILD_DIR/DEBIAN
+
 # Copy binaries
-cp $SRC_PATH/src/komodod $DEB_BIN
-strip $DEB_BIN/komodod
-cp $SRC_PATH/src/komodo-cli $DEB_BIN
-strip $DEB_BIN/komodo-cli
+cp $SRC_PATH/src/komodod $DEB_BIN/hush-komodod
+strip $DEB_BIN/hush-komodod
+cp $SRC_PATH/src/komodo-cli $DEB_BIN/hush-komodo-cli
+strip $DEB_BIN/hush-komodo-cli
+cp $SRC_PATH/src/komodo-tx $DEB_BIN/hush-komodo-tx
+strip $DEB_BIN/hush-komodo-tx
 cp $SRC_PATH/src/hushd $DEB_BIN
-# Copy docs
+cp $SRC_PATH/src/hush-cli $DEB_BIN
+cp $SRC_PATH/src/hush-tx $DEB_BIN
+
+echo "Copying docs"
 #cp $SRC_PATH/doc/release-notes/release-notes-1.0.0.md $DEB_DOC/changelog
 cp $SRC_PATH/contrib/debian/changelog $DEB_DOC
 #cp $SRC_DEB/changelog $DEB_DOC/changelog.Debian
 cp $SRC_DEB/copyright $DEB_DOC
 cp -r $SRC_DEB/examples $DEB_DOC
-# Copy manpages
+
+echo "Copying manpages"
 cp $SRC_DOC/man/komodod.1 $DEB_MAN/hushd.1
 cp $SRC_DOC/man/komodo-cli.1 $DEB_MAN/hush-cli.1
+cp $SRC_DOC/man/komodo-tx.1 $DEB_MAN/hush-tx.1
+
 # Copy bash completion files
 cp $SRC_PATH/contrib/hushd.bash-completion $DEB_CMP/hushd
 cp $SRC_PATH/contrib/hush-cli.bash-completion $DEB_CMP/hush-cli
@@ -66,14 +91,15 @@ gzip --best -n $DEB_DOC/changelog
 #gzip --best -n $DEB_DOC/changelog.Debian
 gzip --best -n $DEB_MAN/hushd.1
 gzip --best -n $DEB_MAN/hush-cli.1
+gzip --best -n $DEB_MAN/hush-tx.1
 
 cd $SRC_PATH/contrib
 
 # Create the control file
-dpkg-shlibdeps $DEB_BIN/komodod $DEB_BIN/komodo-cli
+dpkg-shlibdeps $DEB_BIN/hush-komodod $DEB_BIN/hush-komodo-cli
 dpkg-gencontrol -P$BUILD_DIR -v$DEBVERSION
 
-# Create the Debian package
+echo "Creating the Debian package"
 fakeroot dpkg-deb --build $BUILD_DIR
 cp $BUILD_PATH/$PACKAGE_NAME-$PACKAGE_VERSION-amd64.deb $SRC_PATH
 # Analyze with Lintian, reporting bugs and policy violations
